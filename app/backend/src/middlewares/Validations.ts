fff96b9d4fe9e7ce12d614a36305efacab0eafd8
Lucas Matos <lucaslmatos18@gmail.com>
2023-07-13 23:30:03 -0300

    Req 10 - Criado validações para email e senha

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -5,10 +5,12 @@ import UsersTable from '../database/models/Users';
 class Validations {
   static validateLoginFields(req: Request, res: Response, next: NextFunction): Response | void {
     const { email, password } = req.body;
+    const rg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!email || !password) {
-      return res.status(400).json({
-        message: 'All fields must be filled',
-      });
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    if (!rg.test(email) || password.length <= 5) {
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     next();
   }
