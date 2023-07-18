f9f02ee2f4c7d078ca0cd9ef3d5c869c4dc27ee7
Lucas Matos <lucaslmatos18@gmail.com>
2023-07-17 21:58:45 -0300

    Adicionado teste de integração para rota /login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,24 +1,25 @@
-import sinon from 'sinon';
-import chai, { expect } from 'chai';
-import chaiHttp from 'chai-http';
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
 
-import {app} from '../../app';
-import loginMock from '../mocks/login.mock';
-import UsersModel from '../../database/models/Users';
+import {app} from '../app';
+import loginMock from './mocks/login.mock';
+import UsersModel from '../database/models/Users';
 
 chai.use(chaiHttp);
+
+const { expect } = chai;
 
 describe('POST /login ', async function () { 
   beforeEach(function () { sinon.restore(); });
     it('Login com sucesso ', async function () {
       // Arrange
       const httpRequestBody = loginMock.validBody
-      const stub = UsersModel.build(loginMock.findOne)
-      sinon.stub(UsersModel, 'findOne').resolves(stub);
       // Act
       const httpResponse = await chai.request(app).post('/login').send(httpRequestBody);
       // Assert
-      expect(httpResponse.status).to.equal(200);
+      expect(httpResponse.status).to.be.deep.equal(200);
       expect(httpResponse.body).to.have.key('token');
     });
 });
